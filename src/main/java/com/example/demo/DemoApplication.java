b291eda5f9fced3236d648b4b73e21b226355a46
Andre Duarte <andreduarte@theagilemonkeys.com>
2023-06-10 22:16:08 -0300

    Simple functional initial application

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.*;
+import com.opencsv.*;
+import java.io.FileReader;
 @SpringBootApplication
 public class DemoApplication {
 
@@ -11,3 +13,31 @@ public class DemoApplication {
 	}
 
 }
+
+// Add the controller
+@RestController
+class HelloWorldController{
+	@GetMapping("/")
+	public String hello(){
+		FileReader filereader;
+		try{
+			filereader = new FileReader("test.csv");
+			CSVReader reader = new CSVReader(filereader);
+			String[] nextLine;
+			
+			
+	
+			nextLine = reader.readNext();
+
+			System.out.println(nextLine[1]);
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+
+
+
+		return "Hello World! This is a Spring Boot Application.";
+	}
+}
+
